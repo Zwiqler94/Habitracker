3817a57452c5f510d8868453e50745e77f6a3016
Jake Zwickler <jazwickler@gmail.com>
2020-07-29 18:55:49 -0400

    add habit-list component

--- habitracker/src/app/app-routing.module.ts
+++ habitracker/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NewHabitComponent } from './new-habit/new-habit.component';
+import { MainScreenComponent } from './main-screen/main-screen.component';
+import { HabitListComponent } from './habit-list/habit-list.component';
 
-const routes: Routes = [{path: 'new-habit', component: NewHabitComponent}];
+const routes: Routes = [
+  {path:'habit-list', component: HabitListComponent },
+  {path: 'new-habit', component: NewHabitComponent},
+  {path: '', component:MainScreenComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- habitracker/src/app/app.module.ts
+++ habitracker/src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatToolbarModule } from '@angular/material/toolbar'
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainScreenComponent } from './main-screen/main-screen.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { MatIconModule } from '@angular/material/icon';
+import { HabitListComponent } from './habit-list/habit-list.component';
+
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainScreenComponent
+    MainScreenComponent,
+    HabitListComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
