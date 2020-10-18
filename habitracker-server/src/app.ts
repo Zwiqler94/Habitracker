592c7d3d9e619d3dc6b4e13e40878acfb23b4c2a
Jake Zwickler <jazwickler@gmail.com>
2020-10-18 17:12:12 -0400

    Revert "Revert "[Habitracker-23] add endpoints to get lists""
    This reverts commit c742500c63fc98ab26c59ca3fb2a97d2d9bffa16.

--- habitracker-server/src/app.ts
+++ habitracker-server/src/app.ts
@@ -3,7 +3,10 @@ const app = express();
 import HabitController from './habits/HabitController';
 import HabitListController from './lists/ListController';
 import * as bodyParser from 'body-parser';
+import { HabitReqSchema } from './habits/HabitReqSchema';
+const cors = require('cors');
 
+app.use(cors());
 app.use(bodyParser.json())
 
 
@@ -11,8 +14,12 @@ app.get("/", (req: any, res: any) => {
     res.send("invalid request");
 });
 
-app.post("/habits", (req: any, res: any) => {
+app.post("/habits", HabitReqSchema, (req: any, res: any) => {
     HabitController.create(req, res);
+});
+
+app.get("/habits", HabitReqSchema, (req: any, res: any) => {
+    HabitController.findHabitsByListId(req, res);
 });
 
 app.post("/habits/:id/complete", (req: any, res: any) => {
@@ -27,6 +34,16 @@ app.post("/habit-lists", (req: any, res: any) => {
     HabitListController.create(req, res);
 });
 
+app.get("/habit-lists", (req: any, res: any) => {
+    HabitListController.getAllLists(req, res);
+});
 
+app.get("/habit-lists/:id", (req: any, res: any) => {
+    HabitListController.getList(req, res);
+});
+
+// app.get("/habit-lists/search", (req: any, res: any) => {
+//     HabitListController.searchForList(req, res);
+// });
 
 export { app };
--- habitracker-server/src/lists/List.ts
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from 'mongoose';
-
-const habitListSchema = new Schema({
-    listId: String,
-    name: String,
-    percentCompleted: Number
-})
-
-export const HabitList = model('Lists', habitListSchema)
