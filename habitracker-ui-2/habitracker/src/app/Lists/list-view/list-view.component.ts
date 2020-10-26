8fcaf607d0ac04c8d27e2bd58f6e959ea944d0be
Jake Zwickler <jazwickler@gmail.com>
2020-10-25 21:22:38 -0400

    add fixed version of dynamic table

--- habitracker-ui-2/habitracker/src/app/Lists/list-view/list-view.component.ts
+++ habitracker-ui-2/habitracker/src/app/Lists/list-view/list-view.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { ListService } from 'src/app/services/list.service';
+import { SidenavService } from 'src/app/services/sidenav.service';
+import { IListOverview } from 'src/models/IListOverview';
 
 @Component({
   selector: 'app-list-view',
   templateUrl: './list-view.component.html',
   styleUrls: ['./list-view.component.scss']
 })
+
+
 export class ListViewComponent implements OnInit {
 
-  constructor() { }
+  constructor(private drawer: SidenavService, private lists: ListService) {
+    this.setupLists();
+  }
 
-  ngOnInit(): void {
+  private listData: IListOverview[] = [];
+  private myLists: any[];
+
+  public displayedColumns: string[] = ['listName', 'listPercentageComplete'];
+
+  public unformattedList;
+
+  public dataSource: IListOverview[];
+
+  public userName = 'Jacob';
+
+  public setupLists(): void {
+    this.lists.getLists()
+      .subscribe((data: any) => {
+        this.myLists = data.lists;
+        for (let i = 0; i < this.myLists.length; i++) {
+          this.listData.push({ listName: this.myLists[i].name, listPercentageComplete: this.myLists[i].percentCompleted });
+        }
+        this.dataSource = this.listData;
+      });
+  }
+
+  public goToList(row): void {
+    console.log(row);
+
+  }
+
+  public toggleDrawer(): void {
+    this.drawer.toggleDrawer();
+  }
+
+  public ngOnInit(): void {
+    this.setupLists();
   }
 
 }
--- habitracker-ui-2/habitracker/src/app/app-routing.module.ts
+++ habitracker-ui-2/habitracker/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeScreenComponent } from 'src/app/welcome-screen/welcome-screen.component';
+import { ListViewComponent } from 'src/app/Lists/list-view/list-view.component';
+import { HabitViewComponent } from 'src/app/Habits/habit-view/habit-view.component';
+import { UsersListsViewComponent } from 'src/app/User/users-lists-view/users-lists-view.component';
 
 const routes: Routes = [
-  { path: '', component: WelcomeScreenComponent}
+  { path: '', component: WelcomeScreenComponent },
+  { path: 'users-lists-view', component: UsersListsViewComponent },
+  { path: 'list-view', component: ListViewComponent },
+  { path: 'habit-view', component: HabitViewComponent }
 ];
 
 @NgModule({
